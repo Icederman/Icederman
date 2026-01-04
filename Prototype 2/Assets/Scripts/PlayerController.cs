6ff0e37739d120a2dfe25e09f73522d319e9300c
Safwan <solaimansafwan@gmail.com>
2026-01-04 19:17:47 +0600

    Added boundaries for the player

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     public float horizontalInput;
+    public float xRange = 10.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,6 +21,19 @@ public class PlayerController : MonoBehaviour
 
         // Player movement 
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        // Creating boundaries for the player
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
+        }        
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+        }
 
     }
 }
