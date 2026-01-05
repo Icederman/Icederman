1e81aabb50d249e40a997011ef81170bdeb5498e
Safwan <solaimansafwan@gmail.com>
2026-01-05 20:01:48 +0600

    Finished Challenge 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeInterval = 2.0f;
+    private float nextTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextTime)
+        {
+            nextTime = Time.time + timeInterval;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
