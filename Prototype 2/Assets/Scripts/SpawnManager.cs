b41ca865898d62f8a143f824d4bd566440c01a1f
Safwan <solaimansafwan@gmail.com>
2026-01-05 12:53:41 +0600

    Created Random Spawn Manager and set the camera to orthographic view

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float spawnRangeX = 20;
+    private float spawnRangeZ = 20;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {   
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+
+        }
+
+
+
+    }
+}
+ 
