9723952f0acf26cdc39929ca34d1435fe51b6c71
Safwan <solaimansafwan@gmail.com>
2026-01-08 17:11:25 +0600

    Started Prototype 3, added components to the player and made the player(Rigidbody) jump

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 500);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
