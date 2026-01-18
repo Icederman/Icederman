16c82c3a805b925aca4021b78d5525ce424fd44a
Safwan <solaimansafwan@gmail.com>
2026-01-19 00:23:12 +0600

    Started Mission 5 and finished up to 5.2

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1f;
+
+    public TextMeshProUGUI scoreText;
+    private int score;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+
+        UpdateScore(0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+
+        }
+
+    }
+
+
+   public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+
+    }
+
+}
+
+
+
